bd68ce7cade324ab48540a9a56f1e59a6b38615b
MaheutNicolas <maheutnicolas30@gmail.com>
2026-06-16 18:07:13 +0200

    feat: :sparkles: start responsive : header

--- src/component/navigation/Header.vue
+++ src/component/navigation/Header.vue
@@ -1,23 +1,34 @@
 <template>
   <header class="header">
     <nav class="header__nav">
-
       <RouterLink to="/" class="header__logo">AKASHA</RouterLink>
 
-      <ul class="header__links">
-        <button class="back-btn" @click="back" v-if="hasBackButton">Retours</button>
-      </ul>
+      <button
+        class="header__burger"
+        :class="{ 'is-open': isMenuOpen }"
+        @click="isMenuOpen = !isMenuOpen"
+        :aria-expanded="isMenuOpen"
+        aria-label="Ouvrir le menu"
+      >
+        <span></span><span></span><span></span>
+      </button>
 
-      <div class="header__right">
-        <span class="header__player" v-if="user">{{ user.replace(/"/g, '')  }}</span>
-        <button @click="logout">Deconnexion</button>
+      <div class="header__collapse" :class="{ 'is-open': isMenuOpen }">
+        <div class="header__main">
+          <button class="back-btn" @click="back" v-if="hasBackButton">Retour</button>
+          <span v-else class="desktop"></span>
+          <span class="header__player" v-if="user">{{ user.replace(/"/g, '') }}</span>
+        </div>
+        <div class="header__right">
+          <button @click="logout">Déconnexion</button>
+        </div>
       </div>
-
     </nav>
   </header>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { RouterLink } from 'vue-router';
 import { router } from '@/router';
 import useAuth from '@/function/useAuth';
@@ -26,10 +37,12 @@ const props = defineProps({
   hasBackButton: Boolean
 })
 
+const isMenuOpen = ref(false);
+
 function back() {
-    router.back();
+  router.back();
+  isMenuOpen.value = false;
 }
 
 const { user, logout } = useAuth();
-
 </script>
